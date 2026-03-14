2b8821ffc10bc2a1da80e0df8358b55b3014c459
Ahriman <falcon20333@gmail.com>
2026-03-15 00:18:19 +0200

    The following methods were implemented in the DataProcessor class: get_largest, get_closest, get_asteroids, get_statistics

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ from config import API_KEY, BASE_URL
 neo = parser.NASAParser(API_KEY, BASE_URL)
 data = neo.get_asteroids("2026-03-06", "2026-03-13")
 neo.save_to_json(data, "NEO_asteroids.json")
-print(processor.DataProcessor.get_fastest(data))
+print(processor.DataProcessor.get_statistics(data))
--- processor.py
+++ processor.py
@@ -1,3 +1,5 @@
+from statistics import mean
+
 class DataProcessor:
     @staticmethod
     def get_dangerous(data):
@@ -10,5 +12,28 @@ class DataProcessor:
     
     @staticmethod
     def get_fastest(data):
-        asteroids = [asteroid for value in data["near_earth_objects"].values() for asteroid in value]
-        return max(asteroids, key=lambda x: float(x["close_approach_data"][0]["relative_velocity"]["kilometers_per_second"]))
+        return max(DataProcessor.get_asteroids(data), key=lambda x: float(x["close_approach_data"][0]["relative_velocity"]["kilometers_per_second"]))
+    
+    @staticmethod
+    def get_largest(data):
+        return max(DataProcessor.get_asteroids(data), key=lambda x: x["estimated_diameter"]["meters"]["estimated_diameter_max"])
+
+    @staticmethod
+    def get_closest(data):
+        return min(DataProcessor.get_asteroids(data), key=lambda x: float(x["close_approach_data"][0]["miss_distance"]["astronomical"]))
+    
+    @staticmethod
+    def get_asteroids(data):
+        return [asteroid for value in data["near_earth_objects"].values() for asteroid in value]
+    
+    @staticmethod
+    def get_statistics(data):
+        number_asteroids = len(DataProcessor.get_asteroids(data))
+        number_dangers = len(DataProcessor.get_dangerous(data))
+        average_speed = mean(float(asteroid["close_approach_data"][0]["relative_velocity"]["kilometers_per_second"]) for asteroid in DataProcessor.get_asteroids(data))
+        average_diameter = mean(asteroid["estimated_diameter"]["meters"]["estimated_diameter_max"] for asteroid in DataProcessor.get_asteroids(data))
+        return {"number_of_asteroids": number_asteroids, 
+                "number_of_dangers": number_dangers, 
+                "average_speed": average_speed, 
+                "average_diameter": average_diameter, 
+                "closest_approach_astronomical": DataProcessor.get_closest(data)["close_approach_data"][0]["miss_distance"]["astronomical"]}
